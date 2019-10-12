408b9a8c5064d07dd7d7a20acd3829ea1cd81538
vphoton <voxeledphoton@gmail.com>
2019-10-12 16:19:57 -0400

    added procanimtest scene and spider2 model
    spider2 has fixed armature rotation (was at -90x) also moved fang bones
    around

---
+++ Beat Down 2/Assets/My Assets/Scripts/Spoider/Iktest.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class Iktest : MonoBehaviour
+{
+   public Transform Root;
+   public Transform Mid;
+   public Transform End;
+ 
+   public Transform MidIK;
+   public Transform EndIK;  
+   public float kneeDirection;
+ 
+   Quaternion RootToMidQuat = Quaternion.identity;
+   Quaternion RootToEndQuat = Quaternion.identity;  
+   Quaternion RootStartQuat;
+
+   void Start(){
+       RootStartQuat = Root.rotation;
+   }
+ 
+   void  LateUpdate ()
+   {  
+        float RootToMid = Vector3.Distance(Root.position, Mid.position);
+        float MidToEnd  = Vector3.Distance(Mid.position, End.position);
+        float RootToEnd = Vector3.Distance(EndIK.position, Root.position); //total leg length  
+    
+        RootToEndQuat.SetLookRotation(EndIK.position - Root.position, MidIK.position - Root.position);  
+        //RootToEndQuat = RootStartQuat * Quaternion.Inverse(RootToEndQuat);
+
+    
+        if (Mathf.Abs(RootToMid-MidToEnd) > RootToEnd)
+        {    
+            Mid.localEulerAngles =  new Vector3(Mid.localEulerAngles.x, Mid.localEulerAngles.y, 180);      
+            RootToMidQuat.eulerAngles = new Vector3(0, kneeDirection, 0); //-270 determines direction Knee will face
+        }  
+        else if (Mathf.Abs(RootToMid+MidToEnd) < RootToEnd)
+        {        
+            Mid.localEulerAngles =  new Vector3(Mid.localEulerAngles.x, Mid.localEulerAngles.y, 0);      
+            RootToMidQuat.eulerAngles = new Vector3(0, kneeDirection, 0);  
+        }
+        else
+        {      
+            //uses pythagorean theroem to figure out angles
+            float modz = Mathf.Asin((RootToMid*RootToMid+MidToEnd*MidToEnd-RootToEnd*RootToEnd)/(2*RootToMid*MidToEnd))*Mathf.Rad2Deg + 90;        
+            Mid.localEulerAngles = new Vector3(Mid.localEulerAngles.x, Mid.localEulerAngles.y, modz);        
+            RootToMidQuat.eulerAngles = new Vector3(0, kneeDirection, Mathf.Asin((RootToEnd*RootToEnd+RootToMid*RootToMid-MidToEnd*MidToEnd)/(2*RootToEnd*RootToMid))*Mathf.Rad2Deg - 90);
+        }  
+
+        Root.rotation = RootToEndQuat * RootToMidQuat; //quaternion multiplication will make RootToMidQuat a child to the rotations of RootToEndQuat      
+        End.rotation = EndIK.rotation;  
+   }
+}
