a5007062bacd458387d370ad6fb6984c5a3b2b2b
vphoton <voxeledphoton@gmail.com>
2019-10-13 01:45:24 -0400

    Added Easing script and yet another version of the spider
    IK is in, just need to fix walking

---
+++ Beat Down 2/Assets/My Assets/Scripts/Spoider/LegHome.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LegHome : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+}
