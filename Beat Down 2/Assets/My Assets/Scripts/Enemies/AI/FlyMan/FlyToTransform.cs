8e01946f685de458062b559dd44da0dce3de27ac
Geeknerd1337 <geeknerd1337@gmail.com>
2019-10-28 23:46:33 -0400

    Flying AI

---
+++ Beat Down 2/Assets/My Assets/Scripts/Enemies/AI/FlyMan/FlyToTransform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyToTransform : MonoBehaviour
+{
+
+
+    public Transform target;
+    private Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        transform.SetParent(null);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FollowTargetWithRotation(target, 1f, speed);
+    }
+
+    void FollowTargetWithRotation(Transform target, float distanceToStop, float speed)
+    {
+        if (Vector3.Distance(transform.position, target.position) > distanceToStop)
+        {
+            transform.LookAt(target);
+            rb.AddRelativeForce(Vector3.forward * speed, ForceMode.Force);
+        }
+    }
+
+    void FollowTargetWitouthRotation(Transform target, float distanceToStop, float speed)
+    {
+        var direction = Vector3.zero;
+        if (Vector3.Distance(transform.position, target.position) > distanceToStop)
+        {
+            direction = target.position - transform.position;
+            rb.AddRelativeForce(direction.normalized * speed, ForceMode.Force);
+        }
+    }
+}
---
+++ Beat Down 2/Assets/My Assets/Scripts/Enemies/AI/FlyMan/Navigator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Navigator : MonoBehaviour
+{
+    private float time = 0;
+    public float timer = 3f;
+    private NavMeshAgent agent;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Search();
+    }
+
+    void Search()
+    {
+        time += Time.deltaTime;
+        if (time > timer)
+        {
+            agent.SetDestination(player.position);
+            time = 0;
+
+        }
+
+
+
+    }
+}
