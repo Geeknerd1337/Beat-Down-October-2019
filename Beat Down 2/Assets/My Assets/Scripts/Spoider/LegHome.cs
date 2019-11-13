5b1b5941362b4cfc6c00deeeb6ca60f8c2d0b0dc
vphoton <voxeledphoton@gmail.com>
2019-11-13 00:02:40 -0500

    fixed DungBoi
    wohoo, just need to get the ball rolling

--- Beat Down 2/Assets/My Assets/Scripts/Spoider/LegHome.cs
+++ Beat Down 2/Assets/My Assets/Scripts/Spoider/LegHome.cs
@@ -6,7 +6,7 @@ public class LegHome : MonoBehaviour
 {
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
         transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
@@ -19,10 +19,10 @@ public class LegHome : MonoBehaviour
         if (Physics.Raycast(transform.position + Vector3.up * 2f,//transform.TransformDirection(Vector3.up) * 2f,
                 Vector3.down,//transform.TransformDirection(Vector3.down), 
                 out hit, 5f)){
-            transform.position = hit.point;
+            transform.position = new Vector3(transform.position.x, hit.point.y, transform.position.z);
         }
         //transform.rotation = Quaternion.LookRotation(Vector3.forward,Vector3.up);
         Debug.DrawLine(transform.position + Vector3.up * 2f, transform.position + Vector3.up * 2f + Vector3.down * 5f);
-
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, Vector3.up);
     }
 }
