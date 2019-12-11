8a8d492c9357d11b7c9ce742b22f3ce8f4891127
Geeknerd1337 <geeknerd1337@gmail.com>
2019-12-11 13:13:15 -0500

    Started Working on the Beatle

--- Beat Down 2/Assets/My Assets/Scripts/DungBoi/DungBall.cs
+++ Beat Down 2/Assets/My Assets/Scripts/DungBoi/DungBall.cs
@@ -9,12 +9,34 @@ public class DungBall : MonoBehaviour
     [SerializeField] Transform bumTarget;
     bool hasBall = true;
     bool launchBall = false;
-    float ballSpeed = .5f;
+    float ballSpeed = 1.0f;
     Rigidbody rb;
+    Transform player;
+    bool launched = false;
+
+    public float targetScale;
+    float curScale = 0.1f;
     // Start is called before the first frame update
     void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        transform.localScale = Vector3.one * curScale ;
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+    }
+
+
+    void Update()
+    {
+        curScale += Time.deltaTime * 6f;
+        curScale = Mathf.Clamp(curScale, 0, targetScale);
+        transform.localScale = Vector3.one * curScale;
+
+        if(transform.localScale == (Vector3.one * targetScale))
+        {
+            transform.SetParent(null);
+            launchBall = true;
+        }
     }
 
     // Update is called once per frame
@@ -30,11 +52,11 @@ public class DungBall : MonoBehaviour
                 //rotate butt up when it has the ball
 
                 //Vector3 targRot = bumTarget.rotation * (bumTarget.position - abdomen.position);
-                Vector3 targRot = abdomen.forward;
+                /*Vector3 targRot = abdomen.forward;
                 Quaternion targetRotation = Quaternion.LookRotation(
                     targRot,
                     abdomen.up
-                );
+                );*/
                 /*
                 abdomen.rotation = Quaternion.Slerp(
                     abdomen.rotation,
@@ -42,12 +64,14 @@ public class DungBall : MonoBehaviour
                     1 - Mathf.Exp(-1 * Time.deltaTime)
                 );
                 /*/
-                abdomen.rotation = targetRotation;
+                //abdomen.rotation = targetRotation;
                 //*/
             }
-            if (launchBall)
+            if (launchBall && !launched)
             {
-                rb.AddForce(transform.eulerAngles,ForceMode.Impulse);
+                rb.AddForce(Vector3.right * 1000f);
+                ballHome = player;
+                launched = true;
             }
         }
         else
@@ -57,7 +81,7 @@ public class DungBall : MonoBehaviour
         if (!isClose)
         {
             //rotate butt down when ball isn't close
-            Quaternion targetRotation = Quaternion.LookRotation(
+            /*Quaternion targetRotation = Quaternion.LookRotation(
                 abdomen.forward,
                 abdomen.up
             );
@@ -65,7 +89,7 @@ public class DungBall : MonoBehaviour
                 abdomen.rotation,
                 targetRotation,
                 1 - Mathf.Exp(-1 * Time.deltaTime)
-            );
+            );*/
         }
                 
 
