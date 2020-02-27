4863a170d1f1326962c70073ddeaaf088182f800
Geeknerd1337 <geeknerd1337@gmail.com>
2020-02-27 18:05:56 -0500

    Fixed some issues

---
+++ Beat Down 2/Assets/My Assets/Scripts/Weapons/GrowGrenade.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowGrenade : MonoBehaviour
+{
+    private Vector3 oldScale;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        oldScale = transform.localScale;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (SongManager.ManagerInstance.CheckIfValidTimeWithinFirepattern())
+            {
+                transform.localScale = Vector3.zero;
+            }
+        }
+        transform.localScale = Vector3.Lerp(transform.localScale, oldScale, speed * Time.deltaTime);
+    }
+}
